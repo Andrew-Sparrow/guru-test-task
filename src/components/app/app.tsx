408e46178e984e96a598287f8f53dabc7645f1fd
Andrew <8911102@gmail.com>
2023-03-02 21:02:14 +0500

    adds GlobalStyles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,21 @@
+import styled from 'styled-components';
+import GlobalStyles from '../../styles/global';
+
+const StyledApp = styled.div`
+  width: 320px;
+
+  @media(min-width: 920px) {
+    width: 968px;
+  }
+`;
+
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>Hello, world!</StyledApp>;
+    </>);
 }
 
 export default App;
